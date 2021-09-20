286ca910ee54aa15cd5b9e1042fd88201b5905a4
Gigotaur <90615070+Gigotaur@users.noreply.github.com>
2021-09-20 11:23:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,336 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int age;
+        System.out.println("введите количество лет: ");
+        age = num.nextInt();
+        if (age > 0 && age < 10) {
+            if (age % 10 == 1) {
+                System.out.println("один год");
+            }
+            if (age % 10 == 2) {
+                System.out.println("два года");
+            }
+            if (age % 10 == 3) {
+                System.out.println("три года");
+            }
+            if (age % 10 == 4) {
+                System.out.println("четыре года");
+            }
+            if (age % 10 == 5) {
+                System.out.println("пять лет");
+            }
+            if (age % 10 == 6) {
+                System.out.println("шесть лет");
+            }
+            if (age % 10 == 7) {
+                System.out.println("семь лет");
+            }
+            if (age % 10 == 8) {
+                System.out.println("восемь лет");
+            }
+            if (age % 10 == 9) {
+                System.out.println("девять лет");
+                if (age >= 20 && age < 30) {
+                    System.out.print("двадцать ");
+                    if (age % 10 == 1) {
+                        System.out.println("один год");
+                    }
+                    if (age % 10 == 2) {
+                        System.out.println("два года");
+                    }
+                    if (age % 10 == 3) {
+                        System.out.println("три года");
+                    }
+                    if (age % 10 == 4) {
+                        System.out.println("четыре года");
+                    }
+                    if (age % 10 == 5) {
+                        System.out.println("пять лет");
+                    }
+                    if (age % 10 == 6) {
+                        System.out.println("шесть лет");
+                    }
+                    if (age % 10 == 7) {
+                        System.out.println("семь лет");
+                    }
+                    if (age % 10 == 8) {
+                        System.out.println("восемь лет");
+                    }
+                    if (age % 10 == 9) {
+                        System.out.println("девять лет");
+                    }
+                    if (age % 10 == 0) {
+                        System.out.println("лет");
+                    }
+                }
+                if (age >= 30 && age < 40) {
+                    System.out.print("тридцать ");
+                    if (age % 10 == 1) {
+                        System.out.println("один год");
+                    }
+                    if (age % 10 == 2) {
+                        System.out.println("два года");
+                    }
+                    if (age % 10 == 3) {
+                        System.out.println("три года");
+                    }
+                    if (age % 10 == 4) {
+                        System.out.println("четыре года");
+                    }
+                    if (age % 10 == 5) {
+                        System.out.println("пять лет");
+                    }
+                    if (age % 10 == 6) {
+                        System.out.println("шесть лет");
+                    }
+                    if (age % 10 == 7) {
+                        System.out.println("семь лет");
+                    }
+                    if (age % 10 == 8) {
+                        System.out.println("восемь лет");
+                    }
+                    if (age % 10 == 9) {
+                        System.out.println("девять лет");
+                    }
+                    if (age % 10 == 0) {
+                        System.out.println("лет");
+                    }
+                }
+                if (age >= 40 && age < 50) {
+                    System.out.print("сорок ");
+                    if (age % 10 == 1) {
+                        System.out.println("один год");
+                    }
+                    if (age % 10 == 2) {
+                        System.out.println("два года");
+                    }
+                    if (age % 10 == 3) {
+                        System.out.println("три года");
+                    }
+                    if (age % 10 == 4) {
+                        System.out.println("четыре года");
+                    }
+                    if (age % 10 == 5) {
+                        System.out.println("пять лет");
+                    }
+                    if (age % 10 == 6) {
+                        System.out.println("шесть лет");
+                    }
+                    if (age % 10 == 7) {
+                        System.out.println("семь лет");
+                    }
+                    if (age % 10 == 8) {
+                        System.out.println("восемь лет");
+                    }
+                    if (age % 10 == 9) {
+                        System.out.println("девять лет");
+                    }
+                    if (age % 10 == 0) {
+                        System.out.println("лет");
+                    }
+                }
+                if (age >= 50 && age < 60) {
+                    System.out.print("пятьдесят ");
+                    if (age % 10 == 1) {
+                        System.out.println("один год");
+                    }
+                    if (age % 10 == 2) {
+                        System.out.println("два года");
+                    }
+                    if (age % 10 == 3) {
+                        System.out.println("три года");
+                    }
+                    if (age % 10 == 4) {
+                        System.out.println("четыре года");
+                    }
+                    if (age % 10 == 5) {
+                        System.out.println("пять лет");
+                    }
+                    if (age % 10 == 6) {
+                        System.out.println("шесть лет");
+                    }
+                    if (age % 10 == 7) {
+                        System.out.println("семь лет");
+                    }
+                    if (age % 10 == 8) {
+                        System.out.println("восемь лет");
+                    }
+                    if (age % 10 == 9) {
+                        System.out.println("девять лет");
+                    }
+                    if (age % 10 == 0) {
+                        System.out.println("лет");
+                    }
+                }
+                if (age >= 60 && age < 70) {
+                    System.out.print("шестьдесят ");
+                    if (age % 10 == 1) {
+                        System.out.println("один год");
+                    }
+                    if (age % 10 == 2) {
+                        System.out.println("два года");
+                    }
+                    if (age % 10 == 3) {
+                        System.out.println("три года");
+                    }
+                    if (age % 10 == 4) {
+                        System.out.println("четыре года");
+                    }
+                    if (age % 10 == 5) {
+                        System.out.println("пять лет");
+                    }
+                    if (age % 10 == 6) {
+                        System.out.println("шесть лет");
+                    }
+                    if (age % 10 == 7) {
+                        System.out.println("семь лет");
+                    }
+                    if (age % 10 == 8) {
+                        System.out.println("восемь лет");
+                    }
+                    if (age % 10 == 9) {
+                        System.out.println("девять лет");
+                    }
+                    if (age % 10 == 0) {
+                        System.out.println("лет");
+                    }
+                }
+                if (age >= 70 && age < 80) {
+                    System.out.print("семьдесят ");
+                    if (age % 10 == 1) {
+                        System.out.println("один год");
+                    }
+                    if (age % 10 == 2) {
+                        System.out.println("два года");
+                    }
+                    if (age % 10 == 3) {
+                        System.out.println("три года");
+                    }
+                    if (age % 10 == 4) {
+                        System.out.println("четыре года");
+                    }
+                    if (age % 10 == 5) {
+                        System.out.println("пять лет");
+                    }
+                    if (age % 10 == 6) {
+                        System.out.println("шесть лет");
+                    }
+                    if (age % 10 == 7) {
+                        System.out.println("семь лет");
+                    }
+                    if (age % 10 == 8) {
+                        System.out.println("восемь лет");
+                    }
+                    if (age % 10 == 9) {
+                        System.out.println("девять лет");
+                    }
+                    if (age % 10 == 0) {
+                        System.out.println("лет");
+                    }
+                }
+                if (age >= 80 && age < 90) {
+                    System.out.print("восемьдесят ");
+                    if (age % 10 == 1) {
+                        System.out.println("один год");
+                    }
+                    if (age % 10 == 2) {
+                        System.out.println("два года");
+                    }
+                    if (age % 10 == 3) {
+                        System.out.println("три года");
+                    }
+                    if (age % 10 == 4) {
+                        System.out.println("четыре года");
+                    }
+                    if (age % 10 == 5) {
+                        System.out.println("пять лет");
+                    }
+                    if (age % 10 == 6) {
+                        System.out.println("шесть лет");
+                    }
+                    if (age % 10 == 7) {
+                        System.out.println("семь лет");
+                    }
+                    if (age % 10 == 8) {
+                        System.out.println("восемь лет");
+                    }
+                    if (age % 10 == 9) {
+                        System.out.println("девять лет");
+                    }
+                    if (age % 10 == 0) {
+                        System.out.println("лет");
+                    }
+                }
+                if (age >= 90 && age < 100) {
+                    System.out.print("девяноста ");
+                    if (age % 10 == 1) {
+                        System.out.println("один год");
+                    }
+                    if (age % 10 == 2) {
+                        System.out.println("два года");
+                    }
+                    if (age % 10 == 3) {
+                        System.out.println("три года");
+                    }
+                    if (age % 10 == 4) {
+                        System.out.println("четыре года");
+                    }
+                    if (age % 10 == 5) {
+                        System.out.println("пять лет");
+                    }
+                    if (age % 10 == 6) {
+                        System.out.println("шесть лет");
+                    }
+                    if (age % 10 == 7) {
+                        System.out.println("семь лет");
+                    }
+                    if (age % 10 == 8) {
+                        System.out.println("восемь лет");
+                    }
+                    if (age % 10 == 9) {
+                        System.out.println("девять лет");
+                    }
+                    if (age % 10 == 0) {
+                        System.out.println("лет");
+                    }
+                }
+                switch (age) {
+                    case 10:
+                        System.out.println("десять лет");
+                        break;
+                    case 11:
+                        System.out.println("одиннадцать лет");
+                        break;
+                    case 12:
+                        System.out.println("двенадцать лет");
+                        break;
+                    case 13:
+                        System.out.println("тринадцать лет");
+                        break;
+                    case 14:
+                        System.out.println("четырнадцать лет");
+                        break;
+                    case 15:
+                        System.out.println("пятнадцать лет");
+                        break;
+                    case 16:
+                        System.out.println("шестнадцать лет");
+                        break;
+                    case 17:
+                        System.out.println("семнадцать лет");
+                        break;
+                    case 18:
+                        System.out.println("восемнадцать лет");
+                        break;
+                    case 19:
+                        System.out.println("девятнадцать лет");
+                        break;
+                }
+            }
+        }
+    }
+}
